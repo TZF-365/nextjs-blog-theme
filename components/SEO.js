c01e9bb2ae5ee09e85f07541734d04e25f663494
Technologically Zealous Family <83666532+TZF-365@users.noreply.github.com>
2023-09-07 18:47:17 -0400

    pie

--- components/SEO.js
+++ components/SEO.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export default function SEO({ title, description }) {
   return (
     <Head>
-      <title>{Client Relations Workshop}</title>
+      <title>{}</title>
       <meta name="Silver Tree Agency and Principles of Wealth presents the Client Relations Workshop!" content={description} />
       <meta property="Client Relations Workshop, Improve Client Relations Skills, Mastering Client Relationship Building, Enhance Customer Satisfaction" content={title} />
     </Head>
